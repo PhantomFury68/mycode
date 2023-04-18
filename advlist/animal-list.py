d8b30b1c507580116130a7969aece74e511b2b03
PhantomFury68 <smsaund@yahoo.com>
2023-04-18 16:58:09 +0000

    learning about dictionaries

---
+++ advlist/animal-list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   List - making selections from complex lists"""
+
+def main():
+
+    # create a list called list1
+    list1 = [Fox, Fly, Ant, Bee, Cod, Cat, Dog, Yak, Cow, Hen, Koi, Hog, Jay, Kit]
+
+    Print(list1)
